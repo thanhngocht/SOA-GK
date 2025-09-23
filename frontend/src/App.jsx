5cef04cf797c6e136f717d87c7d44a1168ac6666
Ngoc Huynh <thanhngocbao0304@gmail.com>
2025-09-23 18:14:04 +0700

    fix css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
+import './App.css';
 
 function App() {
   return (
