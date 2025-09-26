449649d0c008e4b9ef89022a8bf902eeffbd34a9
Thao Nguyen <thaonguyen01122024@gmail.com>
2025-09-26 22:29:34 +0700

    Chinh sua App.jsx de chay Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Login />} />
       </Routes>
     </Router>
   );
